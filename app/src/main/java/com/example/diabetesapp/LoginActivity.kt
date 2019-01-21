194cd2acdc9c238a5254fdcd2d423cc33785a748
Callum Majumdar <callum.majumdar@kcl.ac.uk>
2019-01-21 21:52:47 +0000

    implement LoginActivity view basic layout

--- app/src/main/java/com/example/diabetesapp/LoginActivity.kt
+++ app/src/main/java/com/example/diabetesapp/LoginActivity.kt
@@ -1,17 +1,12 @@
 package com.example.diabetesapp
 
-import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
-
+        setContentView(R.layout.activity_login)
     }
 }
